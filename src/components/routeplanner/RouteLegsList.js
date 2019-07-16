1bf35e41d7f6944ca36fcc8ae0e0bdb33a0fe09a
Tom Gowans <tomgowans@users.noreply.github.com>
2019-07-16 20:21:04 +0100

    Removed maps (for now)

--- src/components/routeplanner/RouteLegsList.js
+++ src/components/routeplanner/RouteLegsList.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import MapWrapper from './MapWrapper';
-import RouteLeg from './RouteLeg';
+import React, { Component } from "react";
+// import MapWrapper from "./MapWrapper";
+import RouteLeg from "./RouteLeg";
 
 class RouteLegsList extends Component {
   render() {
     let legsObj = this.props.routeData.legs,
-        routeLegs = '';
+      routeLegs = "";
 
-    routeLegs = legsObj.length ? routeLegs = legsObj.map((data, i) => <RouteLeg key={i} data={data}/> ) : <li>No route legs</li>;
+    routeLegs = legsObj.length ? (
+      (routeLegs = legsObj.map((data, i) => <RouteLeg key={i} data={data} />))
+    ) : (
+      <li>No route legs</li>
+    );
 
     return (
       <ul className="single-route-legs">
-        <MapWrapper paths={ legsObj } />
+        {/* <MapWrapper paths={legsObj} /> */}
 
         {routeLegs}
       </ul>
-    )
+    );
   }
 }
 
